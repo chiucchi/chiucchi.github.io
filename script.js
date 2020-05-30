ad0050f0d5129a0aa0fbaf2ef451e13987383b7f
Chiucchi <42324031+chiucchi@users.noreply.github.com>
2020-05-30 18:17:09 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+ $('#one').click(function() {
+    	$('html, body').animate({scrollTop:$("#examples").offset().top}, 1000);
+  });
+  
+  $('#two').click(function() {
+    	$('html, body').animate({scrollTop:$("#something").offset().top}, 1000);
+  });
+  
+  $('#three').click(function() {
+    	$('html, body').animate({scrollTop:$("#jus").offset().top}, 1000);
+  });
+  
+  $('#four').click(function() {
+    	$('html, body').animate({scrollTop:$("#desgraca").offset().top}, 1000);
+  });
+  
+});  /// end of document ready method do not delete this line  
